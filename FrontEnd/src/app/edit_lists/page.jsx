8303966a1c414fe49e7f9609d25b6612237b9149
LadyAudrey <theaudreygreene@gmail.com>
2023-10-05 13:13:50 -0700

    crating FE/ BE folders

--- FrontEnd/src/app/edit_lists/page.jsx
+++ FrontEnd/src/app/edit_lists/page.jsx
@@ -5,11 +5,11 @@ export default function page() {
       <div className="editing">
         <h2 className="text-green-400">Name of List</h2>
         {/* contains a list of stored Lists, with a button to add and edit each one*/}
-        <div className="flex flex-row data">
+        <div className="flex flex-row data items-start">
           <h3>Item Name</h3>
           {/* button listener to access card component */}
           <img src="/Buttons/Edit.svg" className="btn-fx" alt="edit" />
-          <Card/>
+          <Card />
         </div>
       </div>
     </>
--- FrontEnd/src/app/layout.js
+++ FrontEnd/src/app/layout.js
@@ -4,7 +4,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="h-screen w-screen bg-gradient-to-b from-stone-900 to-stone-950">
-      <img src="/Buttons/Hamburger.svg" className="fixed right-0 top-0 sm:m-5 btn-fx" alt="hamburger menu"/>
+      <img src="/Buttons/Hamburger.svg" className="fixed right-0 top-0 sm:m-5 btn-fx " alt="hamburger menu"/>
         {children}
       </body>
     </html>
