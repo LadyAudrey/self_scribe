3d2bafb3d3e342b80e2ac9716fd6e6e68a917a58
LadyAudrey <theaudreygreene@gmail.com>
2023-11-05 18:29:37 -0800

    pruning components, adding Fx'll and css

--- FrontEnd/src/app/edit_lists copy/page.jsx
+++ FrontEnd/src/app/edit_lists copy/page.jsx
@@ -1,4 +1,4 @@
-import Card from "../components/Card";
+import Card from "../components/EditLists";
 export default function page() {
   return (
     <>
--- FrontEnd/src/app/layout.js
+++ FrontEnd/src/app/layout.js
@@ -1,10 +1,20 @@
 import "./globals.css";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="h-screen w-screen bg-gradient-to-b from-stone-900 to-stone-950">
-      <img src="/Buttons/Hamburger.svg" className="fixed right-0 top-0 sm:m-5 btn-fx " alt="hamburger menu"/>
+      <body className="h-screen w-screen bg-gradient-to-b from-stone-800 to-stone-950 text-white">
+        <Link href="/lists">
+          <Image
+            src="/Buttons/List.svg"
+            height={200}
+            width={200}
+            className="fixed right-0 top-0 sm:m-5 btn-fx"
+            alt="hamburger menu"
+          />
+        </Link>
         {children}
       </body>
     </html>
