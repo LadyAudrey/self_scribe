9bf2b74b7bd09b88ccbadafb49b4cba6e5280879
LadyAudrey <theaudreygreene@gmail.com>
2024-08-06 14:34:07 -0700

    creating task_history Context Tree

--- FrontEnd/src/components/Stats/TaskSet.jsx
+++ FrontEnd/src/components/Stats/TaskSet.jsx
@@ -1,15 +1,27 @@
+import { useContext } from "react";
 import { v4 as uuidv4 } from "uuid";
+
+import { ListsContext } from "../../Contexts/ListsContext";
+import { TasksContext } from "../../Contexts/TasksContext";
+import { TaskHistoryContext } from "../../Contexts/TaskHistoryContext";
 
 import { TaskItem } from "./TaskItem";
 import { VisibleBtn } from "../UI_Pieces/VisibleBtn";
 
 export function TaskSet(props) {
-  const { tasks } = props;
+  const { lists } = useContext(ListsContext);
+  const { tasks } = useContext(TasksContext);
+  const { taskHistory, setTaskHistory } = useContext(TaskHistoryContext);
+
+  console.log("lists", lists);
+  console.log("tasks", tasks);
+  console.log("history", taskHistory);
+
   return (
     <>
-      {tasks.map((taskItem) => {
-        return <TaskItem key={uuidv4()} itemID={taskItem.id} />;
-      })}
+      <TaskHistoryContext.Provider value={{ taskHistory, setTaskHistory }}>
+        <h3>hello</h3>
+      </TaskHistoryContext.Provider>
     </>
   );
 }
